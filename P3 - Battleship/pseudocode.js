e5541f6531545afd982227850e68285cdbc85fa5
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2021-10-20 13:14:23 -0300

    Add files via upload

---
+++ P3 - Battleship/pseudocode.js
@@ -0,0 +1,23 @@
+// START-UP REQUIREMENTS
+
+// Read contents of map.txt to get battleship positions in battleship locations grid - VARIABLE
+
+// SHOW USER GRID - VARIABLE
+// Console.log() player's viewable grid to track guesses;
+
+// SHOW START-UP TEXT;
+// LET'S PLAY BATTLESHIP!!
+// You have 30 missiles to fire to sink all five ships
+// [Bingo-type grid] - A-J row at the top, 1 - 10 column on the side
+// Choose your target (Ex: A1)
+
+// FUNCTIONALITY
+// Contain game in a while loop to keep playing while player still has moves left or has not won - VARIABLE
+
+// Get user input - VARIABLE
+// Check if user input is valid
+// Check battleship locations grid to see if user has a hit or a miss
+// Notify user of result
+// Update user's viewable grid with result
+// Update how many turns/missiles remaining - VARIABLE
+// If all missiles use or all battleship hit, end game
